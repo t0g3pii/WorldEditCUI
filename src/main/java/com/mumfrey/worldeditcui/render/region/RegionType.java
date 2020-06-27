d261af96532c82995f96f5eba8aa39743adc7851
Zach Levis <zml@aoeu.xyz>
2020-06-27 07:59:12 -0700

    Tidy up region and event creation

--- src/main/java/com/mumfrey/worldeditcui/render/region/RegionType.java
+++ src/main/java/com/mumfrey/worldeditcui/render/region/RegionType.java
@@ -1,5 +1,11 @@
 package com.mumfrey.worldeditcui.render.region;
 
+import com.mumfrey.worldeditcui.WorldEditCUI;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
 /**
  * The different types of regions and the classes which we use to display them
  * 
@@ -7,23 +13,25 @@ package com.mumfrey.worldeditcui.render.region;
  */
 public enum RegionType
 {
-	CUBOID    ("cuboid",     "Cuboid",     CuboidRegion.class),
-	POLYGON   ("polygon2d",  "2D Polygon", PolygonRegion.class),
-	ELLIPSOID ("ellipsoid",  "Ellipsoid",  EllipsoidRegion.class),
-	CYLINDER  ("cylinder",   "Cylinder",   CylinderRegion.class),
-	POLYHEDRON("polyhedron", "Polyhedron", PolyhedronRegion.class);
+	CUBOID    ("cuboid",     "Cuboid",     CuboidRegion::new),
+	POLYGON   ("polygon2d",  "2D Polygon", PolygonRegion::new),
+	ELLIPSOID ("ellipsoid",  "Ellipsoid",  EllipsoidRegion::new),
+	CYLINDER  ("cylinder",   "Cylinder",   CylinderRegion::new),
+	POLYHEDRON("polyhedron", "Polyhedron", PolyhedronRegion::new);
+
+	private static final Map<String, RegionType> BY_NAME = new HashMap<>();
 	
 	private final String key;
 	
 	private final String name;
-	
-	private final Class<? extends Region> regionClass;
-	
-	private RegionType(String key, String name, Class<? extends Region> regionClass)
+
+	private final Function<WorldEditCUI, Region> maker;
+
+	private RegionType(String key, String name, Function<WorldEditCUI, Region> maker)
 	{
 		this.key = key;
 		this.name = name;
-		this.regionClass = regionClass;
+		this.maker = maker;
 	}
 	
 	public String getKey()
@@ -35,9 +43,19 @@ public enum RegionType
 	{
 		return this.name;
 	}
-	
-	public Class<? extends Region> getRegionClass()
-	{
-		return this.regionClass;
+
+	public Region make(WorldEditCUI controller) {
+		return this.maker.apply(controller);
+	}
+
+
+	static {
+		for (RegionType type : values()) {
+			BY_NAME.put(type.getKey(), type);
+		}
+	}
+
+	public static RegionType named(String key) {
+		return BY_NAME.get(key);
 	}
 }
