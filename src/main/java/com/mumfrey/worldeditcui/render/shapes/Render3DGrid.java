50a44b43ce4509538bfd9af6a8f0d1867122284b
zml <zml@stellardrift.ca>
2021-07-25 19:21:11 -0700

    Fix double-banding from 3D grid not lining up with 3D box

--- src/main/java/com/mumfrey/worldeditcui/render/shapes/Render3DGrid.java
+++ src/main/java/com/mumfrey/worldeditcui/render/shapes/Render3DGrid.java
@@ -19,6 +19,7 @@ import net.minecraft.util.math.MathHelper;
 public class Render3DGrid extends RenderRegion
 {
 	private static final float CULL_RANGE = 128.0F;
+	private static final double SKIP_THRESHOLD = 0.25f; // don't render another band if there is less than this amount left
 	public static final double MIN_SPACING = 1.0;
 	
 	private Vector3 first, second;
@@ -124,8 +125,14 @@ public class Render3DGrid extends RenderRegion
 			ctx.color(line)
 				.beginLines();
 
-			for (double y = Math.max(y1, -cullAtY) + OFFSET; y <= y2 + OFFSET && y <= cullAtY; y += this.spacing)
+			final double yEnd = Math.min(y2 + OFFSET, cullAtY);
+			for (double y = Math.max(y1, -cullAtY) + OFFSET; y <= yEnd; y += this.spacing)
 			{
+				if (yEnd - y < SKIP_THRESHOLD)
+				{
+					continue;
+				}
+
 				ctx.vertex(x1, y, z2)
 					.vertex(x2, y, z2)
 					.vertex(x1, y, z1)
@@ -135,9 +142,15 @@ public class Render3DGrid extends RenderRegion
 					.vertex(x2, y, z1)
 					.vertex(x2, y, z2);
 			}
-			
-			for (double x = Math.max(x1, -cullAtX); x <= x2 && x <= cullAtX; x += this.spacing)
+
+			final double xEnd = Math.min(x2, cullAtX);
+			for (double x = Math.max(x1, -cullAtX); x <= xEnd; x += this.spacing)
 			{
+				if (xEnd - x < SKIP_THRESHOLD)
+				{
+					continue;
+				}
+
 				ctx.vertex(x, y1, z1)
 					.vertex(x, y2, z1)
 					.vertex(x, y1, z2)
@@ -147,9 +160,15 @@ public class Render3DGrid extends RenderRegion
 					.vertex(x, y1, z1)
 					.vertex(x, y1, z2);
 			}
-			
-			for (double z = Math.max(z1, -cullAtZ); z <= z2 && z <= cullAtZ; z += this.spacing)
+
+			final double zEnd = Math.min(z2, cullAtZ);
+			for (double z = Math.max(z1, -cullAtZ); z <= zEnd; z += this.spacing)
 			{
+				if (zEnd - z < SKIP_THRESHOLD)
+				{
+					continue;
+				}
+
 				ctx.vertex(x1, y1, z)
 					.vertex(x2, y1, z)
 					.vertex(x1, y2, z)
