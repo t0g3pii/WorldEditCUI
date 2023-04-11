d1dd2442e3ebe67d564c6724f64c2ed2f45c103c
zml <zml@stellardrift.ca>
2023-04-10 19:34:21 -0700

    release: prepare for further development on 1.19.1+02

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 group = "org.enginehub.worldeditcui"
-version = "${libs.versions.minecraft.get()}+01"
+version = "${libs.versions.minecraft.get()}+02-SNAPSHOT"
 
 repositories {
     // mirrors:
