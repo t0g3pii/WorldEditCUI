3b7bc150b837f2bd2db4d7bec23526f856119049
zml <zml@stellardrift.ca>
2024-10-19 18:03:09 -0700

    1.21.1

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -23,7 +23,7 @@ sequenceOf(
     "fabric",
     "protocol-common",
     "protocol-fabric",
-    // "protocol-neoforge", // not on pres
+    "protocol-neoforge",
 ).forEach {
     include("worldeditcui-$it")
 }
